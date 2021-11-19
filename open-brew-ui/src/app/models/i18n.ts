2aa2dad92f4b0d8962719fcb78c93e80dd72fd2f
Sven Ulrich <sven.ulrich@datev.de>
2021-11-19 13:22:02 +0100

    use nameof instead of string

--- open-brew-ui/src/app/models/i18n.ts
+++ open-brew-ui/src/app/models/i18n.ts
@@ -1,66 +1,67 @@
-const nameof = <T>(name: Extract<keyof T, string>): string => name;
-
 class I18nTranslation {
 
     constructor(private json: any, private language: string, private category: string) {
-
     }
 
     getTranslation(key: string, plural: boolean = false) {
         try {
             return this.json[this.language][this.category][key].translation;
-
         }
         catch {
-            return `{{${key}}}`;
+            return new Translation(`{{${key}}}`);
         }
     }
 }
 
-
 class Translation {
-    key: string = "";
-    key_plural: string = "";
+    key: string;
+    key_plural: string;
+
+    constructor(key: string = "", key_plural: string = "") {
+        this.key = key;
+        this.key_plural = key_plural;
+    }
 }
 
 class Menu extends I18nTranslation {
-    LANGUAGE: Translation;
-    PRICING: Translation;
-    SIGNUP: Translation;
+    LANGUAGE: Translation = new Translation();
+    PRICING: Translation = new Translation();
+    SIGNUP: Translation = new Translation();
 
     constructor(json: any, language: string) {
-        super(json, language, "menu")
-        this.LANGUAGE = this.getTranslation("LANGUAGE");
-        this.PRICING = this.getTranslation("PRICING");
-        this.SIGNUP = this.getTranslation("SIGNUP");
+        super(json, language, "menu");
+
+        this.LANGUAGE = this.getTranslation(nameof(this.LANGUAGE))
+        this.PRICING = this.getTranslation(nameof(this.PRICING))
+        this.SIGNUP = this.getTranslation(nameof(this.SIGNUP))
     }
 }
 
 class Recipes extends I18nTranslation {
-    WORT: Translation | never = new Translation();
-    MATURE: Translation | never = new Translation();
-    YEAST: Translation | never = new Translation();
-    MASH: Translation | never = new Translation();
-    MALT: Translation | never = new Translation();
-    BREWINGWATER: Translation | never = new Translation();
-    HOP: Translation | never = new Translation();
-    CARBONDIOXIDE: Translation | never = new Translation();
-    BITTERNESS: Translation | never = new Translation();
-    ALCOHOL: Translation | never = new Translation();
+    WORT: Translation = new Translation();
+    MATURE: Translation = new Translation();
+    YEAST: Translation = new Translation();
+    MASH: Translation = new Translation();
+    MALT: Translation = new Translation();
+    BREWINGWATER: Translation = new Translation();
+    HOP: Translation = new Translation();
+    CARBONDIOXIDE: Translation = new Translation();
+    BITTERNESS: Translation = new Translation();
+    ALCOHOL: Translation = new Translation();
 
     constructor(json: any, language: string) {
         super(json, language, "recipe");
 
-        this.WORT = this.getTranslation("WORT");
-        this.MATURE = this.getTranslation("MATURE");
-        this.MASH = this.getTranslation("MASH");
-        this.YEAST = this.getTranslation("YEAST");
-        this.MALT = this.getTranslation("MALT");
-        this.BREWINGWATER = this.getTranslation("BREWINGWATER");
-        this.HOP = this.getTranslation("HOP");
-        this.CARBONDIOXIDE = this.getTranslation("CARBONDIOXIDE");
-        this.BITTERNESS = this.getTranslation("BITTERNESS");
-        this.ALCOHOL = this.getTranslation("ALCOHOL");
+        this.WORT = this.getTranslation(nameof(this.WORT));
+        this.MATURE = this.getTranslation(nameof(this.MATURE))
+        this.MASH = this.getTranslation(nameof(this.MASH))
+        this.YEAST = this.getTranslation(nameof(this.YEAST))
+        this.MALT = this.getTranslation(nameof(this.MALT))
+        this.BREWINGWATER = this.getTranslation(nameof(this.BREWINGWATER))
+        this.HOP = this.getTranslation(nameof(this.HOP))
+        this.CARBONDIOXIDE = this.getTranslation(nameof(this.CARBONDIOXIDE))
+        this.BITTERNESS = this.getTranslation(nameof(this.BITTERNESS))
+        this.ALCOHOL = this.getTranslation(nameof(this.ALCOHOL))
     }
 }
 
@@ -68,7 +69,7 @@ export class i18n {
     menu: Menu = {} as Menu;
     recipes: Recipes = {} as Recipes;
 
-    constructor(private json: any, private language: string) {
+    constructor(json: any, language: string) {
         this.menu = new Menu(json, language);
         this.recipes = new Recipes(json, language);
     }
