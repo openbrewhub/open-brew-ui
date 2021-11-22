4959bf57d1526a701a00d7938f572c61866b7a53
Sven Ulrich <sven.ulrich@datev.de>
2021-11-22 12:59:58 +0100

    please welcome cdk 🚀

---
+++ infrastructure/lib/infrastructure-stack.ts
@@ -0,0 +1,15 @@
+import * as cdk from '@aws-cdk/core';
+// import * as sqs from '@aws-cdk/aws-sqs';
+
+export class InfrastructureStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
