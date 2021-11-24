78630318061e7e81798d9ee6a4afde09a97336b2
René Fehlow <rene.fehlow@web.de>
2021-11-24 21:48:40 +0100

    Only frontend infastructure

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { Bucket, } from '@aws-cdk/aws-s3';
-import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment'
-import { CloudFrontWebDistribution, GeoRestriction, PriceClass, } from '@aws-cdk/aws-cloudfront';
 import { InfrastructureStack, } from '../lib/infrastructure-stack';
 
 const app = new cdk.App();
@@ -21,45 +18,4 @@ const stack = new InfrastructureStack(app, 'InfrastructureStack', {
   env: { account: '387314862676', region: 'eu-central-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
-
-const uiBucket = new Bucket(stack, 'open-brew-ui', {
-  websiteIndexDocument: 'index.html',
-  publicReadAccess: true,
-});
-
-const distribution = new CloudFrontWebDistribution(stack, 'open-brew-ui-dist', {
-  originConfigs: [{
-    s3OriginSource: {
-      s3BucketSource: uiBucket,
-    },
-    behaviors: [{
-      isDefaultBehavior: true,
-    }],
-  }],
-  priceClass: PriceClass.PRICE_CLASS_100,
-  geoRestriction: GeoRestriction.allowlist("DE"),
-  errorConfigurations: [{
-    errorCode: 403,
-    responsePagePath: '/index.html',
-    responseCode: 200
-  },
-  {
-    errorCode: 404,
-    responsePagePath: '/index.html',
-    responseCode: 200
-  }],
-});
-
-const deployment = new BucketDeployment(stack, 'ui-deployment', {
-  sources: [
-    Source.asset('./../dist/open-brew-ui'),
-  ],
-  distribution,
-  destinationBucket: uiBucket,
-  distributionPaths: [
-    '/*'
-  ],
-  prune: true,
-  retainOnDelete: false,
 });
--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,32 +1,51 @@
-import * as path from 'path';
 import * as cdk from '@aws-cdk/core';
-import * as lambda from '@aws-cdk/aws-lambda';
-import * as apigateway from '@aws-cdk/aws-apigateway';
-
-declare const getBookHandler: lambda.Function;
+import { Bucket, } from '@aws-cdk/aws-s3';
+import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment'
+import { CloudFrontWebDistribution, GeoRestriction, PriceClass, } from '@aws-cdk/aws-cloudfront';
 
 export class InfrastructureStack extends cdk.Stack {
 
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    const api = new apigateway.SpecRestApi(this, 'recipes-api', {
-      apiDefinition: apigateway.ApiDefinition.fromAsset('./assets/openapi-definition.yml')
+    const uiBucket = new Bucket(this, 'open-brew-ui', {
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: true,
     });
-
-    /*
-    Ist nicht so geil wie angenommen... müssen das erstmal anlegen und dann exportieren, weil da im swagger aws-spezifische Params stehen müssen.
-
-    z. B.:
-            x-amazon-apigateway-integration:
-            uri:> -
-            arn: aws: apigateway: [secure]: lambda: path/2015-03-31/functions/arn: aws: lambda: [secure]: [secure]: function: ApiLambdaCrudDynamoDBExam-getAllItemsFunction0B7A9-1MLKSKO1RUL3I/invocations
-            passthroughBehavior: when_no_match
-            httpMethod: POST
-            type: aws_proxy
-
-    Also händisch anlegen, exportieren, über nen merge die lambda arns updatend und dann neu ausrollen.... -> https://martinmueller.dev/cdk-swagger-eng/
-    */
-   
+    
+    const distribution = new CloudFrontWebDistribution(this, 'open-brew-ui-dist', {
+      originConfigs: [{
+        s3OriginSource: {
+          s3BucketSource: uiBucket,
+        },
+        behaviors: [{
+          isDefaultBehavior: true,
+        }],
+      }],
+      priceClass: PriceClass.PRICE_CLASS_100,
+      geoRestriction: GeoRestriction.allowlist("DE"),
+      errorConfigurations: [{
+        errorCode: 403,
+        responsePagePath: '/index.html',
+        responseCode: 200
+      },
+      {
+        errorCode: 404,
+        responsePagePath: '/index.html',
+        responseCode: 200
+      }],
+    });
+    
+    const deployment = new BucketDeployment(this, 'ui-deployment', {
+      sources: [
+        Source.asset('./../dist/open-brew-ui'),
+      ],
+      distribution,
+      destinationBucket: uiBucket,
+      distributionPaths: [
+        '/*'
+      ],
+      prune: true,
+      retainOnDelete: false,
+    });
   }
 }
