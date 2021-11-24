19057cae615a558a40f459e4c1c125f07c00ca76
René Fehlow <rene.fehlow@web.de>
2021-11-24 14:01:53 +0100

    SpecApiGateway makes no fun for now

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,15 +1,32 @@
+import * as path from 'path';
 import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigateway from '@aws-cdk/aws-apigateway';
+
+declare const getBookHandler: lambda.Function;
 
 export class InfrastructureStack extends cdk.Stack {
+
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const api = new apigateway.SpecRestApi(this, 'recipes-api', {
+      apiDefinition: apigateway.ApiDefinition.fromAsset('./assets/openapi-definition.yml')
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    /*
+    Ist nicht so geil wie angenommen... müssen das erstmal anlegen und dann exportieren, weil da im swagger aws-spezifische Params stehen müssen.
+
+    z. B.:
+            x-amazon-apigateway-integration:
+            uri:> -
+            arn: aws: apigateway: [secure]: lambda: path/2015-03-31/functions/arn: aws: lambda: [secure]: [secure]: function: ApiLambdaCrudDynamoDBExam-getAllItemsFunction0B7A9-1MLKSKO1RUL3I/invocations
+            passthroughBehavior: when_no_match
+            httpMethod: POST
+            type: aws_proxy
+
+    Also händisch anlegen, exportieren, über nen merge die lambda arns updatend und dann neu ausrollen.... -> https://martinmueller.dev/cdk-swagger-eng/
+    */
+   
   }
 }
